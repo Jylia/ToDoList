ab6408ea6a1c4b1292f2959235c050faa6d1750e
Jylia Grigorchuk <grigorchukj@gmail.com>
2013-06-26 12:33:51 +0300

    #memoization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,13 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user_session
-    return @current_user_session if defined?(@current_user_session)
-    @current_user_session = UserSession.find
+   # return @current_user_session if defined?(@current_user_session)
+    @current_user_session ||= UserSession.find
   end
 
   def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.record
+  #  return @current_user if defined?(@current_user)
+    @current_user ||= current_user_session && current_user_session.record
   end
 
-  def id_user
-    return @id_user if defined?(@id_user)
-    @id_user = current_user.id
-  end
 end
